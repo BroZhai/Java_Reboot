b32c8b491618723479f8ca70021ac8196dcd4e01
Working Tech <k2785233247@gmail.com>
2025-06-24 10:12:10 +0800

    系统的过了一遍琪露诺的算术class(迫真

--- Java_Reboot/Builtin_Classes/Number_and_Math.java
+++ Java_Reboot/Builtin_Classes/Number_and_Math.java
@@ -4,7 +4,7 @@ import java.lang.Math; // 导入 Math类
 
 public class Number_and_Math {
   public static void main(String[] args) {
-    // Number类实验区
+    /* Number类实验区 */
     Number num1 = Double.valueOf(133.14);
     System.out.println("num当 前的值为: " + num1);
     double sussy_number = 150.17;
@@ -29,9 +29,28 @@ public class Number_and_Math {
     System.out.println(d.equals(ia)); // .equals 用于比较两个'数据对象', 返回布尔, Double 11.0 != Integer 11, false
     System.out.println(ia.equals(ib)); // false, Integer 11 != Integer 12;
     System.out.println(ia.equals(ia_copy)); // true, Integer 11 = Integer 11;
-    if(ia.intValue()==d.intValue()){ // ia, d 数字对象均'取整'
+    if(ia.intValue()==d.intValue()){ // ia, d 数字对象均'取整', 用==进行'数值比较' (统一数值后再进行比较)
       System.out.println("ia.intValue()的值 " + ia.intValue() + " 和d.intValue()的值\s" + d.intValue() +" 一致!");
     }
     
+    /* Math类实验区 */
+    System.out.println("\n以下开始为Cirno's Perfect 'Math Class'");
+    // random()默认是[0.0, 1.0)的随机double小数, 可自行灵活转换 + 扩展
+    int random_value = (int) (Math.random()*100) + 1; // [0+1, 99+1] 范围内取值 (1-100)
+    System.out.println("本轮的random_value为: " + random_value);
+
+    double test_value = 4.5;
+    System.out.println("4.5取floor()的值为: " + Math.floor(test_value)); // 4.0
+    System.out.println("4.5取ceil()的值为: " + Math.ceil(test_value)); // 5.0
+    System.out.println("4.5取rint()的值为: " + Math.ceil(test_value)); // rint()小数的四舍五入, 仍返回小数 5.0
+    System.out.println(Math.ceil(test_value) > Math.floor(test_value) ? "4.5的ceil > floor大" : "4.5的ceil < floor (?waht?");
+    System.out.println("返回了上面的比较中'大'的值: "+ Math.max(Math.ceil(test_value), Math.ceil(test_value)));
+    System.out.println(Math.pow(test_value, 2)); // 计算4.5^2, 始终返回Double;
+
+    int cirno = 9;
+    System.out.println("cirno被根号制裁惹: " + Math.sqrt(cirno)); // 3.0, double
+
+    System.err.println("3^2 + 4^2 开根号 = "+ Math.hypot(3.0, 4.0));
   }
+
 }
