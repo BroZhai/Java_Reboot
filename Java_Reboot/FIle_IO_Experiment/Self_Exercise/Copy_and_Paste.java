f7fc59b3f2d139cab053b3803461e790a59db176
Working Tech <k2785233247@gmail.com>
2025-07-08 11:48:35 +0800

    发现了遗漏的正则表达式知识点, 搞清楚了什么是(?!)和(?<!), 补充了对应的笔记

--- Java_Reboot/FIle_IO_Experiment/Self_Exercise/Copy_and_Paste.java
+++ Java_Reboot/FIle_IO_Experiment/Self_Exercise/Copy_and_Paste.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Parameter;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,6 +12,24 @@ import java.util.regex.Pattern;
 public class Copy_and_Paste {
   // 在本Java中, 尝试来自行实现一下 文件/文件夹 相关的功能
 
+  // 方法定义&实现区域
+  public static void create_File(){
+    System.out.println("\n欢迎来到'文件创建'");
+    Scanner user_input = new Scanner(System.in);
+    // Pattern symbol_set = Pattern.compile("^[^\\/:*?\"\\[\\]<>|]$"); // 文件名不能包含 \ / : * ? " < > | (把这些'不要'的符号滤掉)
+    Pattern symbol_set = Pattern.compile(
+        "^[^\\\\/:*?\\\"\\\\[\\\\]<>|]+$"    // 基础字符集（排除控制符和空格）
+        + "(?<!\\.)"                         // 匹配的'文件名'?<!前面 不能以 \ . 开头
+        + "(?!\\s)"                          // 匹配的'文件名'?!后面 不能以! 空格 结尾 (其实Window会处理结尾的空格, 嘻嘻)
+        + "{1,260}"                          // 长度限制
+    );
+    System.out.print("请输入文件名: ");
+    String filename = user_input.nextLine(); // 读取用户输入的文件名
+    Matcher filename_format = symbol_set.matcher(filename);
+    System.out.println("\n所用输入的文件名合法吗? " + filename_format.matches());
+  }
+
+  // 主函数
   public static void main(String[] args) throws IOException {
     String directory = "Java_Reboot/File_IO_Experiment/Self_Exercise";
     File welcome_text = new File(directory,"welcome.txt");
@@ -37,22 +56,54 @@ public class Copy_and_Paste {
       current_line = user_input.nextLine(); // 读取当前行输入
       Matcher number_matcher = single_number.matcher(current_line);
       if(current_line.length()==1 && number_matcher.matches()){ // 用户只输了一个数字, 和matcher的严格匹配对上了, 顺带校验了'输入长度'作为保险
-        System.out.println("输入有效!");
+        // System.out.println("输入有效!");
         break;
       }else{
         System.out.print("输入无效, 请重试: ");
       }
     }
-
-      // String line = user_input.nextLine().trim(); // 读取当前行
-      // // number_matcher = single_number.matcher(line);
-      // System.out.println("当前读到的行内容为: " + line);
-      // System.out.print("请输入下一行内容: ");
-      // line = user_input.nextLine().trim();
-      // System.out.println("\n下一行读到的内容为: " + line);
-      // System.out.println();
     
-    // }
+    switch (current_line) {
+      case "1":
+        Copy_and_Paste.create_File();
+        break;
+
+      case "2":
+        
+        break;
+      case "3":
+        
+        break;
+
+      case "4":
+        
+        break;
+
+      case "5":
+        
+        break;
+      case "6":
+        
+        break;
+      case "7":
+        
+        break;
+
+      case "8":
+        
+        break;
+      case "9":
+        
+        break;
+
+      case "0":
+        System.out.println("\n欢迎下次光临~~");
+        System.exit(0);
+        break;
+                
+      default:
+        break;
+    }
     
   }
 }
