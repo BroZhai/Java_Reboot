f2333a40d988117346216253712cad73c4559263
Working Tech <k2785233247@gmail.com>
2025-07-08 10:45:18 +0800

    终于修好了'输入'判断的异常, 搞了半天并不是RE的锅, 而是.findInLine() 和 Matcher类的作用方式不同, 之前以为功能差不多 XD, 巩固了Scanner类 和 Matcher类的用法

--- Java_Reboot/FIle_IO_Experiment/Self_Exercise/Copy_and_Paste.java
+++ Java_Reboot/FIle_IO_Experiment/Self_Exercise/Copy_and_Paste.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Copy_and_Paste {
@@ -24,15 +25,34 @@ public class Copy_and_Paste {
     }
 
     System.out.print("\n请提供选择对应数字进行操作: ");
-    Scanner user_input = new Scanner(System.in);
-    Pattern single_number = Pattern.compile("[\\d]{1}");
-    boolean range_judge = user_input.findInLine(single_number) == null; // 找不到, true
-    while(range_judge){
-      System.out.print("无效输入, 请重试: ");
-      user_input.nextLine();
-      range_judge = user_input.findInLine(single_number) == null; // 傻帽了, 忘记给break的循环条件赋值了 XD
+    Scanner user_input = new Scanner(System.in); // 这一行并不会立即唤起'输入'!
+    Pattern single_number = Pattern.compile("[0-9]{1}");
+    String current_line;
+    // Matcher number_matcher = single_number.matcher(current_line);
+    // System.out.println("行内容: " + current_line + " 的number_matcher结果: " + number_matcher.matches());
 
+    // boolean range_judge = user_input.findInLine(single_number) == null; // 输入多位数 23 时会'匹配两个', 2 3 给自己挖大坑了
+    
+    while(true){
+      current_line = user_input.nextLine(); // 读取当前行输入
+      Matcher number_matcher = single_number.matcher(current_line);
+      if(current_line.length()==1 && number_matcher.matches()){ // 用户只输了一个数字, 和matcher的严格匹配对上了, 顺带校验了'输入长度'作为保险
+        System.out.println("输入有效!");
+        break;
+      }else{
+        System.out.print("输入无效, 请重试: ");
+      }
     }
+
+      // String line = user_input.nextLine().trim(); // 读取当前行
+      // // number_matcher = single_number.matcher(line);
+      // System.out.println("当前读到的行内容为: " + line);
+      // System.out.print("请输入下一行内容: ");
+      // line = user_input.nextLine().trim();
+      // System.out.println("\n下一行读到的内容为: " + line);
+      // System.out.println();
+    
+    // }
     
   }
 }
