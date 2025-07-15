f3d297e30fa335533a3bb348f383016cb46eef67
Working Tech <k2785233247@gmail.com>
2025-07-15 10:23:04 +0800

    大体温习了HashSet和HashMap, 整理了对应的笔记, 完成了对HashSet的简单实践

---
+++ Java_Reboot/DataType_Experiment/HashSet_and_HashMap.java
@@ -0,0 +1,38 @@
+package Java_Reboot.DataType_Experiment;
+
+import java.util.HashSet; // 导入HashSet类
+import java.util.HashMap; // 导入HashMap类
+
+// 辅助工具类
+import java.util.Arrays;
+import java.util.List;
+
+public class HashSet_and_HashMap {
+  
+  // 在本类中, 我们来玩转一下Hashset和Hashmap
+  public static void main(String[] args) {
+    // HashSet实验区
+    HashSet<String> unique_names = new HashSet<>();
+    unique_names.add("cirno");
+    unique_names.add("IceWings");
+    unique_names.add("DannieL");
+    unique_names.add("Mr-Pancake");
+    unique_names.add("CyanCandy");
+    System.out.println("HashSet现有: " + unique_names.toString()); // 我们发现往HashSet中存入的数据并没有按照我们写入的顺序来存, 因为每个元素都会单独过一遍hash然后被分配到一个特定的桶中
+    if(unique_names.add("cirno")){ // 尝试追加重复元素
+      System.out.println("This is not freaking possible dude :|");
+    }else{
+      System.out.println("往HashSet中再次追加'cirno'时发生了异常, 不允许元素重复!");
+    }
+    System.out.println("当前HashSet的大小为: " + unique_names.size());
+
+    System.out.println("\n现在创建了一个List, 尝试移除List中的交集元素Mr-Pancake, cirno");
+    String[] remove_names = {"Mr-Pancake","cirno"};
+    List<String> remove_list = Arrays.asList(remove_names); // 定义一个List, 一会儿让HashSet移除'List交集'
+    unique_names.removeAll(remove_list);
+    System.out.println("HashSet现有: " + unique_names.toString());
+    System.out.println("当前HashSet的大小为: " + unique_names.size());
+    
+  }
+
+}
