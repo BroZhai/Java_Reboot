b5328ae32e15b0148324048dd11c8074163e92d4
Working Tech <k2785233247@gmail.com>
2025-07-22 13:56:13 +0800

    大致搞明白了Java中的'箭头函数', 整理了lamda表达式的相关笔记, 成功进行了实验

--- Java_Reboot/Lambda_Experiement/Calculator.java
+++ Java_Reboot/Lambda_Experiement/Calculator.java
@@ -1,5 +1,5 @@
 package Java_Reboot.Lambda_Experiement;
 
 public interface Calculator {
-  int calculate(int a, int b); // 定义抽象方法, 一会儿对这个方法进行'花样重写', 但是不implement
+  int calculate(int a, int b); // 定义抽象方法, 一会儿用该接口的对象对这个方法进行'花样定义', 省的implementsc再重写
 }
--- Java_Reboot/Lambda_Experiement/Main.java
+++ Java_Reboot/Lambda_Experiement/Main.java
@@ -3,6 +3,21 @@ package Java_Reboot.Lambda_Experiement;
 public class Main {
 
   public static void main(String[] args) {
+    // 定义了一个'计算器' 接口'对象'
+    Calculator add = (a,b) -> a+b; // 利用'接口对象'对其内部方法实现'花样定义' (lamda实现)
+    Calculator sub = (c,d) -> c-d;
+    Calculator multiply = (num1, num2) -> num1*num2;
+    Calculator devide = (int1, int2) -> {
+      double decimal_value = (double)int1/int2;
+      System.out.println("取得的精准小数为: " + decimal_value);
+      return int1/int2; // {}多行复杂操作需 手动return
+    };
     
-  }
+    System.out.println("加法结果: "+add.calculate(5, 3)); // 8
+    System.out.println("减法结果: "+sub.calculate(5, 3)); // 2
+    System.out.println("乘法结果: "+multiply.calculate(5, 3)); // 15
+    System.out.println("除法结果: "+devide.calculate(5, 3)); // 1 (1.66666...7)
+
+  } // main结束
+
 }
