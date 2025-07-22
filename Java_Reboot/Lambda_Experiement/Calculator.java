e65dae05da3334192e982520e29cbdfc87beaaa6
Working Tech <k2785233247@gmail.com>
2025-07-22 11:57:32 +0800

    简单的了解了下Java 的lamda操作, 和箭头函数很像, 下午回来继续

---
+++ Java_Reboot/Lambda_Experiement/Calculator.java
@@ -0,0 +1,5 @@
+package Java_Reboot.Lambda_Experiement;
+
+public interface Calculator {
+  int calculate(int a, int b); // 定义抽象方法, 一会儿对这个方法进行'花样重写', 但是不implement
+}
---
+++ Java_Reboot/Lambda_Experiement/Main.java
@@ -0,0 +1,8 @@
+package Java_Reboot.Lambda_Experiement;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+  }
+}
