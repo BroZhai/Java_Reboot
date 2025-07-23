31d56206a2c24764689ddf0855d0981ce5064617
Working Tech <k2785233247@gmail.com>
2025-07-23 15:33:01 +0800

    莫名其妙的实现了服务器和客户端的双向通信, 踩了一个.close()的大坑, 准备整理一下思路

--- Java_Reboot/Network_Sockets/Client.java
+++ Java_Reboot/Network_Sockets/Client.java
@@ -1,17 +1,41 @@
 package Java_Reboot.Network_Sockets;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+// Java 网络相关
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
+// Java '流处理'相关
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Client {
   
+  public static String inputStream_to_String(InputStream data){
+    System.out.println();
+    StringBuilder sb = new StringBuilder(5); // 动态接收'字符串'内容
+    InputStreamReader reader = new InputStreamReader(data); // InputStreamReader, 将 输入的'字符流' 转换为 '字节流'
+    char[] string_buffer = new char[1024]; 
+    int read_value; // 本次读取大小
+    int test_counter = 1;
+    try {
+      while ((read_value = reader.read(string_buffer)) != -1) { // 这里的.read()方法会阻塞I/O, 直到收到客户端回应才会继续
+          sb.append(string_buffer,0,read_value);
+          System.out.println("已从InputStream中读取了 " + read_value + " Bytes, 这是第 " + test_counter + " 次");
+          test_counter++;
+          if(sb.toString().contains("\n")) break;
+      }
+    } catch (IOException e) {
+      System.out.println("发生了IO异常!");
+    }
+   
+    return sb.toString();
+  }
+
   public static void main(String[] args) throws IOException, UnknownHostException {
     InetAddress server_ip = InetAddress.getByName("localhost");
     try {
@@ -22,14 +46,23 @@ public class Client {
       Scanner user_input = new Scanner(System.in);
       System.out.print("请输入发送内容: "); // 取得用户输入信息
       String input_content = user_input.nextLine();
-
+      user_input.close();
       
       OutputStream byte_stream = to_server_socket.getOutputStream(); // 取得客户端'输出流', 拿到后用OutputStreamWriter直接写入'字符数据'
       OutputStreamWriter writer = new OutputStreamWriter(byte_stream); // OutputStreamWriter会自动将其转换为'字节'Byte数据进行网络传输
-      writer.write(input_content);
+      writer.write(input_content + "\n");
       writer.flush();
-      writer.close();
-      
+      // writer.close();
+
+      // 一会儿服务器还会有个回应, 这里用个和服务器同款的阻塞I/O耗着
+      System.out.println("正在等待服务器进行回应...");
+
+      InputStream socket_receive_stream = to_server_socket.getInputStream(); // 从服务器socket过来的'输入'
+      String server_reply = inputStream_to_String(socket_receive_stream); // 这里阻塞
+      System.out.println("\n从服务器接收到的回应: " + server_reply);
+
+      // System.out.println("\n客户端Socket已关闭");
+      // to_server_socket.close(); // 完成通信, 关闭Socket
     } catch (Exception e) {
       System.out.println("Connection Refused, 可能是没找到对应的服务器");
     }
--- Java_Reboot/Network_Sockets/Server.java
+++ Java_Reboot/Network_Sockets/Server.java
@@ -1,41 +1,44 @@
 package Java_Reboot.Network_Sockets;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+// Java 网络相关
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+
+// Java '流处理'相关 (网络传的是Byte)
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+
 
 public class Server {
-  public static String inputStream_to_String(InputStream data){
+  public static String inputStream_to_String(InputStream data) throws IOException{
     System.out.println("测试: " + data);
-    StringBuilder sb = new StringBuilder(5);
+    StringBuilder sb = new StringBuilder(5); // 动态接收'字符串'内容
     InputStreamReader reader = new InputStreamReader(data); // InputStreamReader, 将 输入的'字符流' 转换为 '字节流'
-    char[] string_buffer = new char[40]; // 设置读取缓存 (InputStream是'字节'流)
-    int read_value;
+    char[] string_buffer = new char[20]; // 设置读取缓存 (InputStream是'字节'流), 刻意设的很小, 用于测试, 正常是1024
+    int read_value; // 本次读取大小
+    int test_counter = 1;
     try {
-      while ((read_value = reader.read(string_buffer)) != -1) {
-        for(char i : string_buffer){
-          sb.append(i);
-        }
+      while ((read_value = reader.read(string_buffer)) != -1) { // 这里的.read()方法会阻塞I/O, 直到收到客户端回应才会继续
+          sb.append(string_buffer,0,read_value);
+          System.out.println("已从InputStream中读取了 " + read_value + " Bytes, 这是第 " + test_counter + " 次");
+          test_counter++;
+          if(sb.toString().contains("\n")) break;
         // String current_content = Arrays.toString(string_buffer);
         
       }
     } catch (IOException e) {
       System.out.println("发生了IO异常!");
     }
-   
     return sb.toString();
   }
 
-  public static void main(String[] args) throws IOException, UnknownHostException{
+  public static void main(String[] args) throws IOException, UnknownHostException, InterruptedException{
     InetAddress server_ip = InetAddress.getByName("127.0.0.1");
     ServerSocket sv = new ServerSocket(11451, 20, server_ip);
     while(true){
@@ -44,8 +47,22 @@ public class Server {
       System.out.println("接收到来自端口 " + client_socket.getPort() + " 的连接...");
       // 成功获取到客户端的Socket后, 取得该 客户端Socket的'输入流'.getInputStream获取对应信息 (客户端Socket的'输出流' 对应 服务端Socket的'输入流')
       InputStream client_data = client_socket.getInputStream();
-      String data_content = Server.inputStream_to_String(client_data);
-      System.out.println("尝试取得的数据为: " + data_content);
+      String data_content = Server.inputStream_to_String(client_data); // 这个方法中的.read就会阻塞I/O, 直到收到来自客户端的消息
+      System.out.println("\n客户端Socket发来的信息为: " + data_content);
+      
+      // 要想给客户端进行'回复', 就用到服务器Socket的'输出流', 对应客户端那边Socket的输入流
+      System.out.println("\n服务器正在准备回应消息, 3秒后发送...");
+      // Thread.sleep(3000);
+      String server_reply = "你好! 端口 " + client_socket.getPort() + ", 我收到了你的消息: " + data_content + "\n";
+      System.out.println(server_reply);
+      OutputStream server_output_stream = client_socket.getOutputStream();
+      OutputStreamWriter output_writer = new OutputStreamWriter(server_output_stream);
+      output_writer.write(server_reply);
+      output_writer.flush();
+      // output_writer.close();
+      
+
+
     }
     
   }
