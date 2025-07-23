695ae26a923581eefd1b7ce5ebba04768528d519
Working Tech <k2785233247@gmail.com>
2025-07-23 11:48:24 +0800

    成功查资料'自行'勉强的实现了客户端 至 服务端的通信, 同时认识了InputStreamReader 和 OutputStreamWriter这两个类, 之前看InputStream和OutputStream的时候疏忽了 XD

--- Java_Reboot/Network_Sockets/Client.java
+++ Java_Reboot/Network_Sockets/Client.java
@@ -1,17 +1,40 @@
 package Java_Reboot.Network_Sockets;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
   
   public static void main(String[] args) throws IOException, UnknownHostException {
     InetAddress server_ip = InetAddress.getByName("localhost");
-    Socket to_server_socket = new Socket(server_ip,11451); // 这一行在建立完Socket对象后会'直接自行connect' (无需再手动connect)
-    System.out.println("本地的连接端口为: " + to_server_socket.getLocalPort());
-    // to_server_socket.connect(new InetSocketAddress("localhost", 11451), 5000);
-  }
+    try {
+      Socket to_server_socket = new Socket(server_ip,11451); // 这一行在建立完Socket对象后会'直接自行connect' (无需再手动connect)
+      System.out.println("本地的连接端口为: " + to_server_socket.getLocalPort());
+      // to_server_socket.connect(new InetSocketAddress("localhost", 11451), 5000);
+      OutputStream send_msg = to_server_socket.getOutputStream();
+      Scanner user_input = new Scanner(System.in);
+      System.out.print("请输入发送内容: "); // 取得用户输入信息
+      String input_content = user_input.nextLine();
+
+      
+      OutputStream byte_stream = to_server_socket.getOutputStream(); // 取得客户端'输出流', 拿到后用OutputStreamWriter直接写入'字符数据'
+      OutputStreamWriter writer = new OutputStreamWriter(byte_stream); // OutputStreamWriter会自动将其转换为'字节'Byte数据进行网络传输
+      writer.write(input_content);
+      writer.flush();
+      writer.close();
+      
+    } catch (Exception e) {
+      System.out.println("Connection Refused, 可能是没找到对应的服务器");
+    }
+    
+    
+
+  } // main函数结束
 }
--- Java_Reboot/Network_Sockets/Server.java
+++ Java_Reboot/Network_Sockets/Server.java
@@ -1,12 +1,36 @@
 package Java_Reboot.Network_Sockets;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Server {
+  public static String inputStream_to_String(InputStream data){
+    System.out.println("测试: " + data);
+    StringBuilder sb = new StringBuilder(5);
+    InputStreamReader reader = new InputStreamReader(data); // InputStreamReader, 将 输入的'字符流' 转换为 '字节流'
+    char[] string_buffer = new char[1024]; // 设置读取缓存 (InputStream是'字节'流)
+    int read_value;
+    try {
+      while ((read_value = reader.read(string_buffer)) != -1) {
+        String current_content = Arrays.toString(string_buffer);
+        sb.append(current_content);
+      }
+    } catch (IOException e) {
+      System.out.println("发生了IO异常!");
+    }
+   
+    return sb.toString();
+  }
 
   public static void main(String[] args) throws IOException, UnknownHostException{
     InetAddress server_ip = InetAddress.getByName("127.0.0.1");
@@ -15,6 +39,10 @@ public class Server {
       System.out.println("服务器 " + sv.getInetAddress().getHostAddress() + ":" + sv.getLocalPort() + " 已成功建立, 正在等待连接");
       Socket client_socket = sv.accept(); // 服务器接收到客户端连接, accept()并获取客户端连接socket对象
       System.out.println("接收到来自端口 " + client_socket.getPort() + " 的连接...");
+      // 成功获取到客户端的Socket后, 取得该 客户端Socket的'输入流'.getInputStream获取对应信息 (客户端Socket的'输出流' 对应 服务端Socket的'输入流')
+      InputStream client_data = client_socket.getInputStream();
+      String data_content = Server.inputStream_to_String(client_data);
+      System.out.println("尝试取得的数据为: " + data_content);
     }
     
   }
