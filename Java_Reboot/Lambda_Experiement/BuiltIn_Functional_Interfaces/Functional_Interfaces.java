f92ce8f87b3964751d0ad0b5c2026d18779d66fd
Working Tech <k2785233247@gmail.com>
2025-07-24 14:18:43 +0800

    追加了'函数式接口'的实验工程, 准备开工

---
+++ Java_Reboot/Lambda_Experiement/BuiltIn_Functional_Interfaces/Functional_Interfaces.java
@@ -0,0 +1,10 @@
+package Java_Reboot.Lambda_Experiement.BuiltIn_Functional_Interfaces;
+
+public class Functional_Interfaces {
+
+  // 在本class中, 我们来研究一下Java的内置'函数式接口'Functional_Interfaces (就会外面自己定义的Calculator类似, 但是是'内置的')
+  public static void main(String[] args) {
+    
+  }
+  
+}
