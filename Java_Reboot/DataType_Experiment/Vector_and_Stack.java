5f509696d8a4c8b83f3164d1e6e4b74ec133d869
Working Tech <k2785233247@gmail.com>
2025-08-01 11:30:25 +0800

    简单完成了Vector的所有实验, 继续看看Stack

---
+++ Java_Reboot/DataType_Experiment/Vector_and_Stack.java
@@ -0,0 +1,60 @@
+package Java_Reboot.DataType_Experiment;
+
+// 导入Vector类
+import java.util.Vector;
+
+// 导入其他工具类
+import java.util.Arrays;
+import java.util.Enumeration; // 枚举类 
+import java.util.List;
+
+public class Vector_and_Stack {
+
+  // 在本类中, 我们来玩玩 Vector 及其子类 Stack栈
+
+  public static void main(String[] args) {
+
+    /* Vector实验区 */
+    // 创建一个Vector对象
+    Vector<Integer> my_vect = new Vector<>();
+    my_vect.add(0); // 单纯追加元素
+    System.out.println("当前my_vect中有: " + my_vect.get(0)); // Tips: Vector()实现了List接口, 所有这里可以用List的get()方法获取元素, 当然也可以用自己的elementAt();
+    List<Integer> num_list = Arrays.asList(1,3,2);
+    my_vect.addAll(num_list); // 直接追加Collection类对象
+    System.out.print("追加元素后的my_vect为: ");
+    for(Integer i: my_vect){
+      System.out.print(i + " ");
+    }
+    System.out.println("当前的第一个元素是: " + my_vect.firstElement()+ ", 最后一个元素为: " + my_vect.lastElement());
+
+    // 返回Enumeratin枚举类对象
+    my_vect.add(3);
+    Enumeration<Integer> my_enum = my_vect.elements();
+    System.out.print("追加一个元素后返回的Enumeration枚举类对象中有: ");
+    while(my_enum.hasMoreElements()){
+      int current_num = my_enum.nextElement();
+      System.out.print(current_num + " ");
+    }
+    System.out.println();
+
+    // my_vect.remove(2); // 移除索引2 的元素 (3)
+    System.out.print("移除第一个出现的2的结果为: ");
+    my_vect.remove(Integer.valueOf(2)); // 移除'第一个出现的2'
+    for(Integer i: my_vect){
+      System.out.print(i + " ");
+    }
+    System.out.println();
+
+    my_vect.set(0, 9);
+    System.out.print("修改了index 0的元素: ");
+    for(Integer i: my_vect){
+      System.out.print(i + " ");
+    }
+    System.out.println("\n当前Vector的大小: " + my_vect.size());
+
+    /* Stack实验区 */
+    System.out.println("\n现在来看看Stack");
+
+  } // main函数结束 
+  
+}
