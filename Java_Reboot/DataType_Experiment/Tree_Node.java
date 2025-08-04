781135df4b8eb68edb0df1fa61ea5252a13bf90f
Working Tech <k2785233247@gmail.com>
2025-08-04 14:21:47 +0800

    发现自己基本的思路是对的, 问题出在了边维护树边维护线性list, 两边的操作一重叠就导致整个树乱套了 XD

--- Java_Reboot/DataType_Experiment/Tree_Node.java
+++ Java_Reboot/DataType_Experiment/Tree_Node.java
@@ -18,7 +18,28 @@ public class Tree_Node {
     nodeList.set(b_index, tmp_node);
   }
 
-  public static void insert_node(List<myNode> nodeList, myNode newNode, myNode baseNode){
+  public static void insert_node(myNode root, myNode newNode){
+    if (newNode.value < root.value) {
+      if(root.leftNode != null){
+        insert_node(root.leftNode, newNode);
+      }else{
+        root.leftNode = newNode;
+        System.out.println(newNode.value + " 插入到了 " + root.value + " 的左侧");
+        return;
+      }
+    } else if(newNode.value > root.value){
+      if(root.rightNode != null){
+        insert_node(root.rightNode, newNode);
+      }else{
+        root.rightNode = newNode;
+        System.out.println(newNode.value + " 插入到了 " + root.value + " 的右侧");
+        return;
+      }
+    }
+    
+  }
+
+  /*   public static void insert_node(List<myNode> nodeList, myNode newNode, myNode baseNode){
       int cur_index = nodeList.indexOf(baseNode);
       Optional<myNode> cur_left_node = (cur_index*2 + 1 < nodeList.size() ? Optional.ofNullable(nodeList.get(cur_index*2+1)) : Optional.empty()); // 条件: 检查是否超界(node不存在)? node存在: node不存在
       // Tips: ? Optional.ofNullable(nodeList.get(cur_index*2+1)) : Optional.empty() 在括号内 和 括号外 判断都行
@@ -27,6 +48,7 @@ public class Tree_Node {
       // 和当前node进行比较
       if(newNode.value < baseNode.value){ // 小于当前节点
         cur_left_node.ifPresent((left_node) -> { // 左节点存在, 进行进一步的递归比较
+          System.out.println("进入了左递归!");
           insert_node(nodeList, newNode, left_node);
           return;
         });
@@ -38,6 +60,7 @@ public class Tree_Node {
 
       }else if(newNode.value > baseNode.value){
         cur_right_node.ifPresent((right_Node) -> { // 右节点存在, 进一步递归比较
+          System.out.println("进入了右递归");
           insert_node(nodeList, newNode, right_Node);
           return;
         });
@@ -48,13 +71,16 @@ public class Tree_Node {
       }
 
     }
+      */
 
-    public static void show_list(List<myNode> nodeList){
-      System.out.print("当前Node_list中的元素顺序: ");
-      nodeList.forEach((cur_node) -> {
-        System.out.print(cur_node.value + " ");
-      });
-      System.out.println();
+
+    public static void traverse_tree(List<myNode> result_list, myNode root){
+      if(root == null){
+        return; // 反复迭代遍历到了 最根的null, 此时应该终止遍历
+      }
+      traverse_tree(result_list, root.leftNode); // 当前元素先遍历左树
+      result_list.add(root);
+      traverse_tree(result_list, root.rightNode); // 左树完成再去看右树 (右树中又有可能有左树分岔, 优先又走左树遍历去了, 直到最后遇到左树根节点的null才终止, 然后逐层网上看'走过node'的右树)
     }
 
 
@@ -66,6 +92,15 @@ public class Tree_Node {
       }
       myNode leftNode = null;
       myNode rightNode = null;
+
+      // 比较两个node对象的'值'是否一致
+      @Override
+      public boolean equals(Object o){
+        if (this == o) return true;
+        if(o==null || getClass() != o.getClass()) return false; // 比较类型为空 或 类型不匹配
+        myNode comapre_node = (myNode) o;
+        return this.value == comapre_node.value; 
+      }
     }
 
 
@@ -78,32 +113,34 @@ public class Tree_Node {
     myNode node6 = new myNode(35);
     myNode node7 = new myNode(45);
 
-    List<myNode> nodeList = new ArrayList<>(); // 默认排序 
-    nodeList.add(baseNode); // 33
-    insert_node(nodeList, node2, baseNode); // 28
-    // System.out.println(nodeList.get(0).leftNode.value);
-    insert_node(nodeList, node3, baseNode); // 40
-    insert_node(nodeList, node4, baseNode); // 19
-    insert_node(nodeList, node5, baseNode); // 31
-    insert_node(nodeList, node6, baseNode); // 35
-    insert_node(nodeList, node7, baseNode); // 45
-    show_list(nodeList);
+    List<myNode> result_list = new ArrayList<>(); // 展示结果的List
+    insert_node(baseNode, node2);
+    insert_node(baseNode, node3);
+    insert_node(baseNode, node4);
+    insert_node(baseNode, node5);
+    insert_node(baseNode, node6);
+    insert_node(baseNode, node7);
+
+
     // 最终期望排序 19, 28, 31, 33, 35, 40, 45
-    
+    traverse_tree(result_list, baseNode);
+
     System.out.print("请输入要查找的Node: ");
     Scanner user_input = new Scanner(System.in);
     Integer search_value = user_input.nextInt();
     System.out.println("输入的值为: " + search_value);
     // int node_index = nodeList.indexOf(new myNode(search_value)); // 这里因为比较的是'对象的值', 不能用indexOf() XD
-    Optional<myNode> search_node = nodeList.stream().filter( (cur_node) -> {
+    Optional<myNode> search_node = result_list.stream().filter( (cur_node) -> {
       return cur_node.value == search_value;
     }).findFirst(); // 过滤完后 FindFirst()
     
     search_node.ifPresent( (serach_node) -> {
-      System.out.println("取得" + search_node.get().value + "的信息");
-      System.out.println("左节点: " + search_node.get().leftNode.value);
-      System.out.println("右节点 : " + search_node.get().rightNode.value);
+      System.out.println("\n取得" + serach_node.value + "的信息");
+      System.out.println("左节点: " + serach_node.leftNode.value);
+      System.out.println("右节点: " + serach_node.rightNode.value);
+      return;
     } );
+    System.out.println("\n输入的值不存在!");
     
 
   } // main函数结束
