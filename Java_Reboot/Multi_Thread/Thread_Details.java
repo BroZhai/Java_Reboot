78dedba8d26bceec72b313169817bebc8c517073
Working Tech <k2785233247@gmail.com>
2025-08-05 14:45:26 +0800

    踩了一个setPriority()的坑, setPriority只是将Thread就绪队列中的优先级排到最高而已 XD, 不会抢断当前CPU的时间片

--- Java_Reboot/Multi_Thread/Thread_Details.java
+++ Java_Reboot/Multi_Thread/Thread_Details.java
@@ -12,26 +12,33 @@ public class Thread_Details {
 
   public static void main(String[] args) throws InterruptedException{
     System.out.println("这里是main函数的首行打印消息");
-    Thread random_thread = new MyThread();
-    random_thread.start(); // 出现位置'随机' 
+    Thread random_thread = new MyThread(); // 默认名称 Thread-0 (可用 random_thread.setName("自定义")
+    random_thread.start(); // 出现位置'随机' (优先级和main相同)
 
     System.out.println("main函数中间部分");
 
     Thread my_thread = new Thread( () -> { // lambda 写 Runnable的 Thread
       try {
-        Thread.sleep(8000); // 最常的休眠时间, 确保该线程'留在最后'被执行 (最长的'等待被执行'时间)
-        System.out.println("诶嘿,我是 "+ Thread.currentThread().getName() +", 我的8等待时间结束了, 轮到我在最后给大伙收个尾! ");
+        Thread.sleep(7000); // 最长的休眠时间, 确保该线程'留在最后'被执行 (最长的'等待被执行'时间)
+        System.out.println("诶嘿,我是 "+ Thread.currentThread().getName() +", 我从开始到现在共等了7秒, 轮到我在最后给大伙收个尾! ");
       } catch (InterruptedException e) {
         System.out.println("线程 "+ Thread.currentThread().getName() + "被意外中断!");
       }
       
     });
+    my_thread.setName("Longest_wait_thread"); // 重命名 最后'首尾'的Thread
     my_thread.start();
     // System.out.println("main函数结束");
 
+    Thread top_thread = new Thread( () -> {
+      System.out.println("我是最高优先级的Thread, 按理来说, 我应该会第一时间被最先处理 :3"); // 确实, 但只是位于'就绪队列'的最高优先级, 并不会'抢断'当前CPU划分的时间片, 只是让CPU知道时间片结束后要优先处理你 (大概率出现在'分隔符'后)
+    });
+    top_thread.setPriority(10);
+    top_thread.start();
+
     System.out.println("----------------分隔符------------------");
     // 实验一下join()方法
-    System.out.println("main: 我先执行, 但是马上就会有个哥们用join()插我的队, 我得等他执行完毕才能继续...");
+    System.out.println("main: 我执行后, 马上就会有个哥们用join()插我的队, 我得等他执行完毕才能继续...");
     Thread suspend_thread = new Thread( () -> {
       System.out.println("诶嘿, 我是时延Thread, 外面的main要等我执行4秒执行完, 才可以继续:3"); // 紧跟着'插队提示'
       try {
@@ -41,9 +48,11 @@ public class Thread_Details {
       } 
     });
     suspend_thread.start();
+    System.out.println("当前仍活跃的线程数共有: " + Thread.activeCount()); // 3个线程 : main自身, 4秒延迟的suspend_thread, 7(4+3)秒延迟的my_threa
     suspend_thread.join(); // 这里 suspend_thread() 插了 main的队, suspend_thread()执行完后才会继续跑main()
 
     System.out.println("main: 我靠这家伙总算跑完了xwx"); // main等待suspend_thread() 跑完后才执行的语句
+
 
   } // main结束
 
