6054acc3dfa4c4104ec22ece67473c57b36f9088
Working Tech <k2785233247@gmail.com>
2025-08-06 17:56:19 +0800

    简单了解了一下wait 和 notify方法, 回来继续进行实践

---
+++ Java_Reboot/Multi_Thread/Synchronize_Experiement/Wait_and_Notify.java
@@ -0,0 +1,9 @@
+package Java_Reboot.Multi_Thread.Synchronize_Experiement;
+
+public class Wait_and_Notify {
+  // 我们来研究一下 wait(), notify() 和 notifyALl()
+
+  public static void main(String[] args) {
+    
+  }
+}
