e23efc8283ff0b0d8624d2ef9d980511828a842c
Working Tech <k2785233247@gmail.com>
2025-08-06 11:55:12 +0800

    优化了print逻辑, 更直观了, 一会儿继续研究'同步方法'

--- Java_Reboot/Multi_Thread/Synchronize_Experiement/Basic_Synchronize.java
+++ Java_Reboot/Multi_Thread/Synchronize_Experiement/Basic_Synchronize.java
@@ -4,18 +4,20 @@ public class Basic_Synchronize {
   // 我们来研究一下基础的锁的创建, 以及Thread中的synchronized(){}临界区
   // 这里用一个最简单的例子, 送奶工 和 喝奶工 XD , 奶箱中空时送奶工送奶, 有有奶时喝奶工去喝
   
-  static class PutMilk extends Thread{ // 送奶工, 每隔1秒送一次奶, 若奶箱子有奶 则 等待消费...
+  static class PutMilk extends Thread{ // 送奶工, 若奶箱子有奶 则 等待消费... (但是执行时可能会'反复轮回到送奶工', 由CPU调度决定)
     public void run(){
       while(!isInterrupted()){
         synchronized(MilkBox.box_lock){ // 读取'奶箱锁', 若正被喝奶工使用则等待, 没人用时则检查'奶箱是否为空', 为空则放奶
-        // 没有人在用奶箱, 检查是否有奶
-        if(MilkBox.milk_counts !=0){ // 有奶
-          System.out.println("送奶工: 咦? 奶箱有奶, 哥们先不放了...");
           try {
-            Thread.sleep(5000); // 送奶工等5秒后查看时候还有奶
+            Thread.sleep(2000); // 送奶工等待2秒
           } catch (Exception e) {
             System.out.println("送奶工在sleep期间被中断!");
           }
+          System.out.println("现在轮到'送奶工'放奶");
+
+        // 没有人在用奶箱, 检查是否有奶
+        if(MilkBox.milk_counts !=0){ // 有奶
+          System.out.println("送奶工: 咦? 奶箱有奶, 哥们先不放了...");
         }else{ // 没有奶
           MilkBox.milk_counts++;
           System.out.println("送奶工: 奶箱为空, 哥们放奶了!");
@@ -31,15 +33,17 @@ public class Basic_Synchronize {
   static class DrinkMilk extends Thread{
     public void run(){
       while (!isInterrupted()) {
-        synchronized(MilkBox.box_lock){// 读取'奶箱锁', 若被'送奶工'使用 则 等待
+        synchronized(MilkBox.box_lock){// 读取'奶箱锁', 若被'送奶工'使用 则 等待 (可能'反复轮回喝奶工', 同上)
         // 没有人在用奶箱, 检查是否有奶, 有则消耗
-        if(MilkBox.milk_counts == 0){ // 没有奶
-          System.out.println("喝奶工: 哎我的奶呢??!");
           try {
-            Thread.sleep(1500); // 喝奶工等1.5秒
+            Thread.sleep(2000); // 喝奶工等1.5秒
           } catch (Exception e) {
             System.out.println("喝奶工在sleep期间被中断!");
           }
+        System.out.println("现在轮到了'喝奶的'");
+
+        if(MilkBox.milk_counts == 0){ // 没有奶
+          System.out.println("喝奶工: 哎我的奶呢??!");
         }else{  // 有奶
           MilkBox.milk_counts--;
           System.out.println("喝奶工: 咕噜咕噜咕噜~濠河!!");
@@ -61,6 +65,7 @@ public class Basic_Synchronize {
   public static void main(String[] args) {
     Thread producer = new PutMilk();
     Thread consumer = new DrinkMilk();
+    System.out.println("请等待送奶工/喝奶工开始...约2秒\n");
     producer.start();
     consumer.start();
 
