c9481825ea685cd1daf782c119022c3676aa28fe
Working Tech <k2785233247@gmail.com>
2025-08-07 11:55:33 +0800

    简单的搭好了框架, 回来继续填充, 搞定后继续研究其他锁

--- Java_Reboot/Multi_Thread/ReentrantLock/ReentrantLock_Experiment.java
+++ Java_Reboot/Multi_Thread/ReentrantLock/ReentrantLock_Experiment.java
@@ -20,10 +20,23 @@ class Consumer extends Thread{
 class MilkBox{
   private static final Lock my_lock = new ReentrantLock();
   private final Condition cond = my_lock.newCondition(); // 利用Lock对象的newCondition()方法 创建一个 Condition实例, 利用实例的.await() / .signal() 和 .signalAll()实现 wait(), notify(), notifyAll()方法
+
+  private int milk_counts = 0;
+  
+  public void put_milk(){
+    try{
+      my_lock.lock(); // 手动上锁
+      // my_lock.lockInterruptibly(); // 上'可以在等待期间被中断' 的 锁 (需要单独写 '等待时中断' 的处理逻辑 catch(InterruptedException e){...})
+      // 上锁成功后的'临界区'
+
+    }finally{
+      my_lock.unlock();
+    }
+  }
 }
 
 public class ReentrantLock_Experiment {
-  // 我们来研究一下ReenTracntLock (高级synchronized), 重写一下一个送奶员 和 两个送奶工
+  // 我们来研究一下ReenTracntLock (高级synchronized), 重写一下 一个送奶员 和 两个消费者
 
   public static void main(String[] args) {
     
